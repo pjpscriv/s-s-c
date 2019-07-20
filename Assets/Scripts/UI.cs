3e41bfd67ac3d60650699d94faab0b5701a83ebb
pjpscriv <pjpscriv@gmail.com>
2019-07-20 17:43:15 +1200

    UI is now reading from Core

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UI : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class UI : MonoBehaviour {
 
-    // Update is called once per frame
+    // Core
+    // publice Image coreImg;
+
+    // Module Bars
+    public Image tempImg;
+    public Image aiImg;
+    public Image elecImge;
+    public Image shieldImg;
+    public Image plantImg;
+    public Image portalImg;
+
     void Update()
     {
-        
+        updateValues();
+    }
+
+    void updateValues() {
+        Core core = GameObject.FindGameObjectWithTag("GameController").GetComponent<Core>();
+
+        // coreHealth = core.CoreHP / 100.0;
+        tempImg.fillAmount    = core.Temperature / 100.0f;
+        aiImg.fillAmount      = core.AI / 100.0f;
+        elecImge.fillAmount   = core.Electrical / 100.0f;
+        shieldImg.fillAmount  = core.Shields / 100.0f;
+        plantImg.fillAmount   = core.Plants / 100.0f;
+        portalImg.fillAmount  = core.Portals / 100.0f;
+
+        Debug.Log("UI values updated. :)");
     }
 }
