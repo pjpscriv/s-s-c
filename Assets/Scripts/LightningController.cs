b2c3a7c4a589b543bb0da5db263455f24a54b2e4
Jack <jackbannistersutton@gmail.com>
2019-07-20 15:39:28 +1200

    Updated lighting Controller

---
+++ Assets/Scripts/LightningController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingController : MonoBehaviour
+{
+    //Dab on the MF Commit button
+    // Start is called before the first frame update
+    public Light NorthEastLight1;
+    public Light NorthEastLight2;
+
+    private float nextActionTime = 0.0f;
+    public float period = 0.1f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+
+        }
+    }
+}
